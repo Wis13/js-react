d0634a477f32deeda325fad901f01f45e4071be8
Vadym Koroliuk <vkqa@outlook.com>
2023-02-24 21:48:47 -0800

    change file script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-let number = 5;
-const leftBorderWith = 1;
+const numberOfFilms = +prompt("How many films have you watched?", '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Which was the last movie you watched?', ''),
+      b = prompt('How much will you rate it?', ''),
+      c = prompt('Which was the last movie you watched?', ''),
+      d = prompt('How much will you rate it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
